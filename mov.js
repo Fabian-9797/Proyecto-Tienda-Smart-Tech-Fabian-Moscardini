9085097f897a118c41621f87ef382a1968097e43
unknown <fabian.moscardini@gmail.com>
2024-05-26 00:57:51 -0300

    Agregando proyecto

---
+++ mov.js
@@ -0,0 +1,120 @@
+function dinero () {
+  document.getElementById("Cuota_uno_computadora").innerHTML = "6 cuotas de $ 300.000";
+}
+
+function dinero_tot_uno () {
+  document.getElementById("Cuota_uno_computadora").innerHTML = "Precio total: $ 3.706.999";
+}
+
+function dinero_compu_dos () {
+  document.getElementById("Cuota_dos_computadora").innerHTML = "7 cuotas de $ 250.000";
+}
+
+function dinero_tot_compu_dos () {
+  document.getElementById("Cuota_dos_computadora").innerHTML = "Precio total: $ 4.050.000";
+}
+
+function dinero_compu_tres () {
+  document.getElementById("Cuota_tres_computadora").innerHTML = "3 cuotas de $ 200.000";
+}
+
+function dinero_tot_compu_tres () {
+  document.getElementById("Cuota_tres_computadora").innerHTML = "Precio total: $ 2.925.999";
+}
+
+function dinero_compu_cuatro () {
+  document.getElementById("Cuota_cuatro_computadora").innerHTML = "5 cuotas de $ 400.000";
+}
+
+function dinero_tot_compu_cuatro () {
+  document.getElementById("Cuota_cuatro_computadora").innerHTML = "Precio total: $ 2.322.400";
+}
+
+function dinero_compu_cinco () {
+  document.getElementById("Cuota_cinco_computadora").innerHTML = "3 cuotas de $ 510.000";
+}
+
+function dinero_tot_compu_cinco () {
+  document.getElementById("Cuota_cinco_computadora").innerHTML = "Precio total: $ 3.444.999";
+}
+
+function dinero_notebook_uno () {
+  document.getElementById("Cuota_uno_notebook").innerHTML = "6 cuotas de $ 500.000";
+}
+
+function dinero_tot_notebook_uno () {
+  document.getElementById("Cuota_uno_notebook").innerHTML = "Precio total: $ 3.605.000";
+}
+
+function dinero_notebook_dos () {
+  document.getElementById("Cuota_dos_notebook").innerHTML = "6 cuotas de $ 100.000";
+}
+
+function dinero_tot_notebook_dos () {
+  document.getElementById("Cuota_dos_notebook").innerHTML = "Precio total: $ 623.725";
+}
+
+function dinero_notebook_tres () {
+  document.getElementById("Cuota_tres_notebook").innerHTML = "7 cuotas de $ 110.000";
+}
+
+function dinero_tot_notebook_tres () {
+  document.getElementById("Cuota_tres_notebook").innerHTML = "Precio total: $ 757.500";
+}
+
+function dinero_tablet_uno () {
+  document.getElementById("Cuota_uno_tablet").innerHTML = "3 cuotas de $ 50.000";
+}
+
+function dinero_tot_tablet_uno () {
+  document.getElementById("Cuota_uno_tablet").innerHTML = "Precio total: $ 150.000";
+}
+
+function dinero_tablet_dos () {
+  document.getElementById("Cuota_dos_tablet").innerHTML = "3 cuotas de $ 120.000";
+}
+
+function dinero_tot_tablet_dos () {
+  document.getElementById("Cuota_dos_tablet").innerHTML = "Precio total: $ 380.400";
+}
+
+function dinero_celular_uno () {
+  document.getElementById("Cuota_uno_celu").innerHTML = "6 cuotas de $ 130.000";
+}
+
+function dinero_tot_celular_uno () {
+  document.getElementById("Cuota_uno_celu").innerHTML = "Precio total: $ 695.999";
+}
+
+function dinero_celular_dos () {
+  document.getElementById("Cuota_dos_celu").innerHTML = "3 cuotas de $ 200.000";
+}
+
+function dinero_tot_celular_dos () {
+  document.getElementById("Cuota_dos_celu").innerHTML = "Precio total: $ 600.000";
+}
+
+function dinero_celular_tres () {
+  document.getElementById("Cuota_tres_celu").innerHTML = "11 cuotas de $ 102.000";
+}
+
+function dinero_tot_celular_tres () {
+  document.getElementById("Cuota_tres_celu").innerHTML = "Precio total: $ 1.164.999";
+}
+
+function dinero_celular_cuatro () {
+  document.getElementById("Cuota_cuatro_celu").innerHTML = "10 cuotas de $ 120.000";
+}
+
+function dinero_tot_celular_cuatro () {
+  document.getElementById("Cuota_cuatro_celu").innerHTML = "Precio total: $ 1.140.000";
+}
+
+function dinero_celular_cinco () {
+  document.getElementById("Cuota_cinco_celu").innerHTML = "4 cuotas de $ 35.000";
+}
+
+function dinero_tot_celular_cinco () {
+  document.getElementById("Cuota_cinco_celu").innerHTML = "Precio total: $ 175.999";
+}
+
